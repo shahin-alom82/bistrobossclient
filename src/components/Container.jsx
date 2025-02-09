3ba587352f46681e7413071795697eefb0310d0a
shahinalom <shahinalom3511371@gmail.com>
2025-02-09 20:27:04 +0500

    update

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,7 +4,6 @@ const Container = ({ children, className }) => {
       return (
             <div className={twMerge("max-w-screen-xl mx-auto px-4 lg:px-0", className)}>
                   {children}
-                  {/* l */}
             </div>
       );
 };
