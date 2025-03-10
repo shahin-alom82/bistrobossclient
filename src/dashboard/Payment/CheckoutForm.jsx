86ffa090daf63371af73dd210f06c3b0a4397ecd
shahinalom <shahinalom3511371@gmail.com>
2025-03-10 14:39:54 +0600

    payment update

--- src/dashboard/Payment/CheckoutForm.jsx
+++ src/dashboard/Payment/CheckoutForm.jsx
@@ -1,8 +1,56 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import { MdPayment } from "react-icons/md";
 
 const CheckoutForm = () => {
+      const [error, setError] = useState('')
+      const stripe = useStripe()
+      const elements = useElements()
+      const handleSubmit = async (event) => {
+            event.preventDefault();
+
+            if (!stripe || !elements) {
+                  return
+            }
+            const card = elements.getElement(CardElement)
+            if (card === null) {
+                  return
+            }
+            // else {
+
+            // }
+
+      }
       return (
-            <div>
-                  
+            <div className="mt-10">
+                  <div className="flex items-center gap-4 text-gray-800">
+                        <MdPayment size={35} />
+                        <h1 className="lg:text-2xl"> Enter Payment Details</h1>
+                  </div>
+                  <form className="max-w-screen-sm mt-6" onSubmit={handleSubmit}>
+                        <CardElement
+
+                              options={{
+                                    style: {
+                                          base: {
+
+                                                fontSize: '18px',
+                                                color: '#424770',
+                                                '::placeholder': {
+                                                      color: '#aab7c4',
+                                                },
+                                          },
+                                          invalid: {
+                                                color: '#9e2146',
+                                          },
+                                    },
+                              }}
+                              className="p-3 border-2 border-yellow-600 focus:outline-none focus:ring mb-4"
+                        />
+                        <button type="submit" className="bg-yellow-500 w-20 py-1 mt-3 text-xl text-gray-700" disabled={!stripe}>
+                              Pay
+                        </button>
+                  </form>
             </div>
       );
 };
--- src/dashboard/Payment/Payment.jsx
+++ src/dashboard/Payment/Payment.jsx
@@ -1,14 +1,15 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
 // TODO :
-const stripePromise = loadStripe()
+const stripePromise = loadStripe(import.meta.env.VITE_payment_gateway_pk)
 const Payment = () => {
       return (
             <div>
-                  <h1 className="text-2xl tracking-wide text-yellow-600">--- Payment History ---</h1>
+                  <h1 className="lg:text-2xl tracking-wide text-yellow-600">--- Payment History ---</h1>
                   <div>
                         <Elements stripe={stripePromise}>
-
+                              <CheckoutForm />
                         </Elements>
                   </div>
             </div>
